693639256c39ed5dc2b200fd61b766484ef3fcc5
elaayy <aelishapearlroncesvalles.candido25@bicol-u.edu.ph>
2026-09-11 16:29:54 +0800

    Updated LoginForm in Program.cs of BillingSystem and Implemented login validation with Users table in BillingDB

--- BillingSystem/LoginForm.Designer.cs
+++ BillingSystem/LoginForm.Designer.cs
@@ -93,6 +93,7 @@
             btnLogin.TabIndex = 5;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
             // 
             // btnCancel
             // 
